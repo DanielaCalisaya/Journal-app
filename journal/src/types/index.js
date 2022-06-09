213b1e062e3023b30e662be1ec69c0dc9b5d9229
DanielaCalisaya <hdaniela127c@gmail.com>
2022-06-08 19:45:08 -0500

    structure folders

---
+++ journal/src/types/index.js
@@ -0,0 +1,19 @@
+export const types = {  /* acciones */
+
+    login : '[Auth] Login',
+    logout : '[Auth] Logout',
+
+    uiSetError : '[UI] Set error',  /* Seteamos el error */
+    uiRemoveError : '[UI] Remove error',
+    uiStartLoading : '[UI] Start loading',
+    uiFinishLoading : '[UI] Finish loading',
+
+    notesAddNew : '[Notes] New note',  /* listado de notas */
+    noteActive : '[Notes] Set active note',
+    notesLoad : '[Notes] Load notes',
+    noteUpdated : '[Notes] Updated note',
+    noteDeleted : '[Notes] Deleted note',
+    noteFileUrl : '[Notes] Updated image url',
+    noteLogoutCleaning : '[Notes] Logout cleaning'
+
+}
